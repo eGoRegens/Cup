b6953fcf313f3a1a829e94b8e54c2863d75a5ebe
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 22:18:28 +0400

    установка констант

--- src/main/java/Cup.java
+++ src/main/java/Cup.java
@@ -1,11 +1,11 @@
 public class Cup {
     public static void main(String[] args) {
-        int glassVolumeMilliliters = 200;
-        double glassFull = 51.2;
-        double volumeWater = (glassVolumeMilliliters * glassFull) / 100;
+        final int Glass_Volume_Milliliters = 200;
+        final double  Glass_Full = 51.2;
+        double volumeWater = (Glass_Volume_Milliliters * Glass_Full) / 100;
 
-        System.out.println("Объем стакана - " + glassVolumeMilliliters + " мл.");
-        System.out.println("Стакан заполнен в " + glassFull + "%");
+        System.out.println("Объем стакана - " + Glass_Volume_Milliliters + " мл.");
+        System.out.println("Стакан заполнен в " + Glass_Full + "%");
         System.out.println("Сейчас в стакане " + volumeWater + " мл воды.");
     }
 }
