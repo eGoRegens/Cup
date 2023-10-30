27c203ee6e38cdd5e4022030f296763b5cc22ef1
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 20:15:19 +0400

    add task4

---
+++ src/main/java/Cup.java
@@ -0,0 +1,12 @@
+public class Cup {
+    public static void main(String[] args) {
+        int glassVolumeMilliliters = 200;
+        double glassFull = 51.2;
+        double volumeWater = (glassVolumeMilliliters * glassFull) / 100;
+
+        System.out.println("Объем стакана - " + glassVolumeMilliliters + " мл.");
+        System.out.println("Стакан заполнен в " + glassFull + "%");
+        System.out.println("Сейчас в стакане " + volumeWater + " мл воды.");
+
+    }
+}
